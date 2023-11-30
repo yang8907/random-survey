29e2e2e88cf882fb48123e4160361d0280aba0d8
yang8907 <152467832+yang8907@users.noreply.github.com>
2023-11-30 14:45:25 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,6 +9,6 @@
 
 const urls = [
 	'https://hhming.moe',
-	'https://forms.gle/6TpE4t3juUfGdsDn6',
-	'https://forms.gle/dBN3yHuM7LRWr8bk6'
+	'https://forms.gle/h3PaJUL1yzzHG2P77',
+	'https://forms.gle/3xDG4SPz8fbhADWr9'
 ];
