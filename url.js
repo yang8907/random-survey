5fb7141ca83c967586932cdae58cc47e78260b7a
yang8907 <152467832+yang8907@users.noreply.github.com>
2024-03-12 09:00:39 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,12 +8,10 @@
 
 
 const urls = [
-	'https://forms.gle/WKnUAbJKPk83mVaE9',
-	'https://forms.gle/9NQUC4jMzuVS3rMd8',
-	'https://forms.gle/pk2A8WBbLXkiRnH6A',
-        'https://forms.gle/6A6zpp12LTdexLiH7',
-        'https://forms.gle/K5aqAAMEZ7iHosKe8',
-        'https://forms.gle/QwE7sa6XJBHmU6dK9',
-        'https://forms.gle/jg1nYhW78aRzbGnp6',
-        'https://forms.gle/jaDm2ZzfVjSergof9'
+	'https://www.surveycake.com/s/RvRM8',
+	'https://www.surveycake.com/s/bydQ9',
+	'https://www.surveycake.com/s/4dAZQ',
+        'https://www.surveycake.com/s/LdAgY',
+        'https://www.surveycake.com/s/3zAVr',
+        'https://www.surveycake.com/s/yOD49',
 ];
