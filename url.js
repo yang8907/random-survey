11d3bc2b1b0d0da66e121c0a3b9149d14f69d08d
yang8907 <152467832+yang8907@users.noreply.github.com>
2024-03-15 22:07:42 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,10 +8,9 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/RvRM8',
 	'https://www.surveycake.com/s/Y8kv7',
-	'https://www.surveycake.com/s/Ognda',
 	'https://www.surveycake.com/s/bydYg',
 	'https://www.surveycake.com/s/LdAZ2',
-	'https://www.surveycake.com/s/3zAwR'
+	'https://www.surveycake.com/s/3zAwR',
+	'https://www.surveycake.com/s/RvRM8'
 ];
